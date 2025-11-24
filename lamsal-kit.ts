cc539d6316b1be4f146d141059190b0f5f6bf791
Roman Lamsal <roman@lamsal.de>
2025-11-24 17:40:01 +0100

    feat: add version flag and switch to tsup

--- lamsal-kit.ts
+++ lamsal-kit.ts
@@ -2,6 +2,11 @@
 
 import process from "node:process"
 
+if (process.argv.includes("--version")) {
+    await import("./package.json").then(({ version }) => console.log("Version:", version))
+    process.exit(0)
+}
+
 if (process.argv[2] === "init") {
     await import("./cli/init")
 }
